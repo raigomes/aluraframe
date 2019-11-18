89f0c3fa56c96e24dff258c0d295d68c33999155
raigomes <raigomes2@hotmail.com>
2019-11-18 00:47:16 -0300

    Criando ProxyFactory para isolar criação do Proxy

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,34 +1,18 @@
 class NegociacaoController {
     constructor() {
         const $ = document.querySelector.bind(document)
-        const self = this
+
         this._inputData = $('#data')
         this._inputQuantidade = $('#quantidade')
         this._inputValor = $('#valor')
 
-        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
-            get(target, prop, receiver) {
-                if (['adiciona', 'apaga'].includes(prop) && typeof (target[prop]) === 'function') {
-
-                    return function () {
-
-                        Reflect.apply(target[prop], target, arguments)
-                        self._negociacoesView.update(target)
-                    }
-                }
-                return Reflect.get(target, prop, receiver)
-            }
-
-        })
+        this._listaNegociacoes = ProxyFactory.create(
+            new ListaNegociacoes(), ['adiciona', 'apaga'], model => this._negociacoesView.update(model))
         this._negociacoesView = new NegociacoesView($('#negociacoesView'))
         this._negociacoesView.update(this._listaNegociacoes)
 
-        this._mensagem = new Proxy(new Mensagem(), {
-            set(target, prop, value, receiver) {
-                Reflect.set(target, prop, value, receiver)
-                self._mensagemView.update(target)
-            }
-        })
+        this._mensagem = ProxyFactory.create(
+            new Mensagem(), ['texto'], model => this._mensagemView.update(model))
         this._mensagemView = new MensagemView($('#mensagemView'))
         this._mensagemView.update(this._mensagem)
     }
