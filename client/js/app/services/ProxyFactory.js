fde2d32fbf2f48e9c678b6f086d3b445a90bbfd5
raigomes <raigomes2@hotmail.com>
2019-11-18 01:19:01 -0300

    Ajustando retorno dos métodos do Proxy

--- client/js/app/services/ProxyFactory.js
+++ client/js/app/services/ProxyFactory.js
@@ -4,18 +4,17 @@ class ProxyFactory {
             get(target, prop, receiver) {
                 if (props.includes(prop) && ProxyFactory._ehFuncao(target[prop])) {
                     return function () {
-                        Reflect.apply(target[prop], target, arguments)
-                        return acao(target)
+                        let response = Reflect.apply(target[prop], target, arguments)
+                        acao(target)
+                        return response
                     }
                 }
                 return Reflect.get(target, prop, receiver)
             },
             set(target, prop, value, receiver) {
-                if (props.includes(prop)) {
-                    target[prop] = value
-                    acao(target)
-                }
-                return Reflect.set(target, prop, value, receiver)
+                let response = Reflect.set(target, prop, value, receiver)
+                if (props.includes(prop)) acao(target)
+                return response
             }
         })
     }
