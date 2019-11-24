9d6bf8944d654d607ecb86f72ba6d8ec74b797ce
raigomes <raigomes2@hotmail.com>
2019-11-24 02:44:38 -0300

    Refatorando NegociacaoService

--- client/js/app/services/NegociacaoService.js
+++ client/js/app/services/NegociacaoService.js
@@ -5,26 +5,38 @@ class NegociacaoService {
     obterNegociacoesDaSemana() {
         return this._service
             .get('/negociacoes/semana')
-            .then(resposta =>
-                resposta.map(item => new Negociacao(new Date(item.data), item.quantidade, item.valor))
-            )
-            .catch(erro => console.log('Não foi possível importar as negociações da semana', erro))
+            .then(negociacoes => {
+                console.log(negociacoes)
+                return negociacoes.map(item => new Negociacao(new Date(item.data), item.quantidade, item.valor))
+            })
+            .catch(erro => {
+                console.log(erro)
+                throw new Error('Não foi possível importar as negociações da semana')
+            })
 
     }
     obterNegociacoesDaSemanaAnterior() {
         return this._service
             .get('/negociacoes/anterior')
-            .then(resposta =>
-                resposta.map(item => new Negociacao(new Date(item.data), item.quantidade, item.valor))
-            )
-            .catch(erro => console.log('Não foi possível importar as negociações da semana anterior', erro))
+            .then(negociacoes => {
+                console.log(negociacoes)
+                return negociacoes.map(item => new Negociacao(new Date(item.data), item.quantidade, item.valor))
+            })
+            .catch(erro => {
+                console.log(erro)
+                throw new Error('Não foi possível importar as negociações da semana')
+            })
     }
     obterNegociacoesDaSemanaRetrasada() {
         return this._service
             .get('/negociacoes/retrasada')
-            .then(resposta =>
-                resposta.map(item => new Negociacao(new Date(item.data), item.quantidade, item.valor))
-            )
-            .catch(erro => console.log('Não foi possível importar as negociações da semana retrasada', erro))
+            .then(negociacoes => {
+                console.log(negociacoes)
+                return negociacoes.map(item => new Negociacao(new Date(item.data), item.quantidade, item.valor))
+            })
+            .catch(erro => {
+                console.log(erro)
+                throw new Error('Não foi possível importar as negociações da semana')
+            })
     }
 }
