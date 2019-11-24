d08fb6372cb91d8d05a0891f863cfd5f96c25c10
raigomes <raigomes2@hotmail.com>
2019-11-24 02:39:01 -0300

    Incluindo método post

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -20,4 +20,24 @@ class HttpService {
         })
 
     }
+
+    post(url, dado) {
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest()
+
+            xhr.open("POST", url, true)
+            xhr.setRequestHeader("Content-type", "application/json")
+            xhr.onreadystatechange = () => {
+
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        resolve(JSON.parse(xhr.responseText))
+                    } else {
+                        reject(xhr.responseText)
+                    }
+                }
+            }
+            xhr.send(JSON.stringify(dado))
+        });
+    }
 }
