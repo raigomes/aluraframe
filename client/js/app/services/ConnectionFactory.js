543d44751ee9f1d901000bade601a1d4e66e21d0
raigomes <rai.mesquita@infoglobo.com.br>
2020-10-25 20:05:51 -0300

    Encapsulando conteúdo em uma função auto invocada para que somente a classe seja pública

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -1,49 +1,53 @@
-var stores = ['negociacoes']
-var version = 4
-var dbName = 'aluraframe'
+var ConnectionFactory = (function () { 
+    
+    var stores = ['negociacoes']
+    var version = 4
+    var dbName = 'aluraframe'
 
-// Necessário para que seja criada somente UMA conexão
-var connection = null
+    // Necessário para que seja criada somente UMA conexão
+    var connection = null
 
-class ConnectionFactory {
+    return class ConnectionFactory {
 
-    constructor() {
-        throw new Error('Não é possível criar instâncias de ConnectionFactory')
+        constructor() {
+            throw new Error('Não é possível criar instâncias de ConnectionFactory')
+        }
+
+        static getConnection() {
+            return new Promise((resolve, reject) => {
+
+                let openRequest = window.indexedDB.open(dbName, version)
+
+                openRequest.onupgradeneeded = e => {
+
+                    ConnectionFactory._createStores(e.target.result)
+                }
+
+                openRequest.onsuccess = e => {
+
+                    if(!connection) connection = e.target.result
+                    resolve(connection)
+                }
+
+                openRequest.onerror = e => {
+
+                    console.log(e.target.error)
+                    reject(e.target.error.name)
+                }
+
+            })
+        }
+
+        static _createStores(connection) {
+
+            stores.forEach(store => {
+
+                if(connection.objectStoreNames.contains(store))
+                    connection.deleteObjectStore(store)
+
+                connection.createObjectStore(store, { autoIncrement: true })
+            })
+        }
     }
 
-    static getConnection() {
-        return new Promise((resolve, reject) => {
-
-            let openRequest = window.indexedDB.open(dbName, version)
-
-            openRequest.onupgradeneeded = e => {
-
-                ConnectionFactory._createStores(e.target.result)
-            }
-
-            openRequest.onsuccess = e => {
-
-                if(!connection) connection = e.target.result
-                resolve(connection)
-            }
-
-            openRequest.onerror = e => {
-
-                console.log(e.target.error)
-                reject(e.target.error.name)
-            }
-
-        })
-    }
-
-    static _createStores(connection) {
-
-        stores.forEach(store => {
-
-            if(connection.objectStoreNames.contains(store))
-                connection.deleteObjectStore(store)
-
-            connection.createObjectStore(store, { autoIncrement: true })
-        })
-    }
-}
+})();
