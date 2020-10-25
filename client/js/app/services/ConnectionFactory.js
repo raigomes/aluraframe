6dabeb8a31d43c9e2d9992fdac4fad11c93f1fae
raigomes <rai.mesquita@infoglobo.com.br>
2020-10-25 19:45:15 -0300

    Criando classe ConnectionFactory.js que retorna uma Promise e retorna uma, e somente uma, conexão compartilhada

---
+++ client/js/app/services/ConnectionFactory.js
@@ -0,0 +1,49 @@
+var stores = ['negociacoes']
+var version = 4
+var dbName = 'aluraframe'
+
+// Necessário para que seja criada somente UMA conexão
+var connection = null
+
+class ConnectionFactory {
+
+    constructor() {
+        throw new Error('Não é possível criar instâncias de ConnectionFactory')
+    }
+
+    static getConnection() {
+        return new Promise((resolve, reject) => {
+
+            let openRequest = window.indexedDB.open(dbName, version)
+
+            openRequest.onupgradeneeded = e => {
+
+                ConnectionFactory._createStores(e.target.result)
+            }
+
+            openRequest.onsuccess = e => {
+
+                if(!connection) connection = e.target.result
+                resolve(connection)
+            }
+
+            openRequest.onerror = e => {
+
+                console.log(e.target.error)
+                reject(e.target.error.name)
+            }
+
+        })
+    }
+
+    static _createStores(connection) {
+
+        stores.forEach(store => {
+
+            if(connection.objectStoreNames.contains(store))
+                connection.deleteObjectStore(store)
+
+            connection.createObjectStore(store, { autoIncrement: true })
+        })
+    }
+}
